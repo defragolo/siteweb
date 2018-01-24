03e8c5c01b291930d1fcdf71e91d74cf171a16af
tsuchigumoo <35763044+tsuchigumoo@users.noreply.github.com>
2018-01-24 12:09:27 +0100

    Add files via upload

---
+++ paged'acceuil.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <link rel="stylesheet" href="style1.css">
+   <meta charset="UTF-8">
+<title> organs4life </title>
+<link rel="stylesheet" href="style1.css">
+</head>
+<?php
+include 'menu.php'
+?>
+
+
+</html>
